4eb0f00e71199915893e461e6f7be1432c39c94b
will_123456 <15816776+will123456890@user.noreply.gitee.com>
2025-05-21 14:10:10 +0800

    init

---
+++ .wrangler/tmp/dev-End1sf/index.js
@@ -0,0 +1,287 @@
+var __defProp = Object.defineProperty;
+var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+
+// .wrangler/tmp/bundle-cl5n6A/checked-fetch.js
+var urls = /* @__PURE__ */ new Set();
+function checkURL(request, init) {
+  const url = request instanceof URL ? request : new URL(
+    (typeof request === "string" ? new Request(request, init) : request).url
+  );
+  if (url.port && url.port !== "443" && url.protocol === "https:") {
+    if (!urls.has(url.toString())) {
+      urls.add(url.toString());
+      console.warn(
+        `WARNING: known issue with \`fetch()\` requests to custom HTTPS ports in published Workers:
+ - ${url.toString()} - the custom port will be ignored when the Worker is published using the \`wrangler deploy\` command.
+`
+      );
+    }
+  }
+}
+__name(checkURL, "checkURL");
+globalThis.fetch = new Proxy(globalThis.fetch, {
+  apply(target, thisArg, argArray) {
+    const [request, init] = argArray;
+    checkURL(request, init);
+    return Reflect.apply(target, thisArg, argArray);
+  }
+});
+
+// .wrangler/tmp/bundle-cl5n6A/strip-cf-connecting-ip-header.js
+function stripCfConnectingIPHeader(input, init) {
+  const request = new Request(input, init);
+  request.headers.delete("CF-Connecting-IP");
+  return request;
+}
+__name(stripCfConnectingIPHeader, "stripCfConnectingIPHeader");
+globalThis.fetch = new Proxy(globalThis.fetch, {
+  apply(target, thisArg, argArray) {
+    return Reflect.apply(target, thisArg, [
+      stripCfConnectingIPHeader.apply(null, argArray)
+    ]);
+  }
+});
+
+// index.js
+async function handleRequest(request, env) {
+  const url = new URL(request.url);
+  let imagePrompt = url.searchParams.get("t");
+  if (!imagePrompt) {
+    return new Response("miss params", { status: 400 });
+  }
+  let imageBuffer = await env.IMAGE_KV.get("image-" + imagePrompt);
+  if (!imageBuffer) {
+    try {
+      let transRes = await translate(imagePrompt, env);
+      console.log(transRes);
+      imagePrompt = transRes.result.translated_text;
+    } catch (e) {
+      console.log("\u7FFB\u8BD1\u9519\u8BEF:");
+      console.log(e);
+    }
+    console.log("\u63D0\u793A\u8BCD\uFF1A", imagePrompt);
+    imageBuffer = await generateImageWithCloudflare(imagePrompt, env);
+  }
+  return new Response(imageBuffer, {
+    headers: { "Content-Type": "image/png", "Cache-Control": "public, max-age=1800" }
+  });
+}
+__name(handleRequest, "handleRequest");
+async function translate(text, env, input, output) {
+  return await fetch(`https://api.cloudflare.com/client/v4/accounts/${env.account_id}/ai/run/@cf/meta/m2m100-1.2b`, {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${env.SF_TOKEN}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      text,
+      source_lang: input ? input : "chinese",
+      target_lang: output ? output : "english"
+    })
+  }).then((res) => res.json());
+}
+__name(translate, "translate");
+async function generateImageWithCloudflare(prompt, env) {
+  const response = await fetch(`https://api.cloudflare.com/client/v4/accounts/${env.account_id}/ai/run/${env.CF_IMG2TEXT_MODEL}`, {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${env.SF_TOKEN}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      model: env.CF_IMG2TEXT_MODEL,
+      // 使用 Cloudflare 的模型生成图像
+      prompt
+      // num_steps: env.FLUX_NUM_STEPS
+    })
+  });
+  if (!response.ok) {
+    throw new Error("\u751F\u6210\u56FE\u50CF\u5931\u8D25");
+  }
+  const imageBuffer = await response.arrayBuffer();
+  const key = `image-${prompt}`;
+  await env.IMAGE_KV.put(key, imageBuffer, { expirationTtl: 1800 });
+  return imageBuffer;
+}
+__name(generateImageWithCloudflare, "generateImageWithCloudflare");
+var index_default = {
+  async fetch(request, env, ctx) {
+    return handleRequest(request, env);
+  }
+};
+
+// ../../../../opt/homebrew/lib/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts
+var drainBody = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx) => {
+  try {
+    return await middlewareCtx.next(request, env);
+  } finally {
+    try {
+      if (request.body !== null && !request.bodyUsed) {
+        const reader = request.body.getReader();
+        while (!(await reader.read()).done) {
+        }
+      }
+    } catch (e) {
+      console.error("Failed to drain the unused request body.", e);
+    }
+  }
+}, "drainBody");
+var middleware_ensure_req_body_drained_default = drainBody;
+
+// ../../../../opt/homebrew/lib/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts
+function reduceError(e) {
+  return {
+    name: e?.name,
+    message: e?.message ?? String(e),
+    stack: e?.stack,
+    cause: e?.cause === void 0 ? void 0 : reduceError(e.cause)
+  };
+}
+__name(reduceError, "reduceError");
+var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx) => {
+  try {
+    return await middlewareCtx.next(request, env);
+  } catch (e) {
+    const error = reduceError(e);
+    return Response.json(error, {
+      status: 500,
+      headers: { "MF-Experimental-Error-Stack": "true" }
+    });
+  }
+}, "jsonError");
+var middleware_miniflare3_json_error_default = jsonError;
+
+// .wrangler/tmp/bundle-cl5n6A/middleware-insertion-facade.js
+var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+  middleware_ensure_req_body_drained_default,
+  middleware_miniflare3_json_error_default
+];
+var middleware_insertion_facade_default = index_default;
+
+// ../../../../opt/homebrew/lib/node_modules/wrangler/templates/middleware/common.ts
+var __facade_middleware__ = [];
+function __facade_register__(...args) {
+  __facade_middleware__.push(...args.flat());
+}
+__name(__facade_register__, "__facade_register__");
+function __facade_invokeChain__(request, env, ctx, dispatch, middlewareChain) {
+  const [head, ...tail] = middlewareChain;
+  const middlewareCtx = {
+    dispatch,
+    next(newRequest, newEnv) {
+      return __facade_invokeChain__(newRequest, newEnv, ctx, dispatch, tail);
+    }
+  };
+  return head(request, env, ctx, middlewareCtx);
+}
+__name(__facade_invokeChain__, "__facade_invokeChain__");
+function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
+  return __facade_invokeChain__(request, env, ctx, dispatch, [
+    ...__facade_middleware__,
+    finalMiddleware
+  ]);
+}
+__name(__facade_invoke__, "__facade_invoke__");
+
+// .wrangler/tmp/bundle-cl5n6A/middleware-loader.entry.ts
+var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
+  constructor(scheduledTime, cron, noRetry) {
+    this.scheduledTime = scheduledTime;
+    this.cron = cron;
+    this.#noRetry = noRetry;
+  }
+  static {
+    __name(this, "__Facade_ScheduledController__");
+  }
+  #noRetry;
+  noRetry() {
+    if (!(this instanceof ___Facade_ScheduledController__)) {
+      throw new TypeError("Illegal invocation");
+    }
+    this.#noRetry();
+  }
+};
+function wrapExportedHandler(worker) {
+  if (__INTERNAL_WRANGLER_MIDDLEWARE__ === void 0 || __INTERNAL_WRANGLER_MIDDLEWARE__.length === 0) {
+    return worker;
+  }
+  for (const middleware of __INTERNAL_WRANGLER_MIDDLEWARE__) {
+    __facade_register__(middleware);
+  }
+  const fetchDispatcher = /* @__PURE__ */ __name(function(request, env, ctx) {
+    if (worker.fetch === void 0) {
+      throw new Error("Handler does not export a fetch() function.");
+    }
+    return worker.fetch(request, env, ctx);
+  }, "fetchDispatcher");
+  return {
+    ...worker,
+    fetch(request, env, ctx) {
+      const dispatcher = /* @__PURE__ */ __name(function(type, init) {
+        if (type === "scheduled" && worker.scheduled !== void 0) {
+          const controller = new __Facade_ScheduledController__(
+            Date.now(),
+            init.cron ?? "",
+            () => {
+            }
+          );
+          return worker.scheduled(controller, env, ctx);
+        }
+      }, "dispatcher");
+      return __facade_invoke__(request, env, ctx, dispatcher, fetchDispatcher);
+    }
+  };
+}
+__name(wrapExportedHandler, "wrapExportedHandler");
+function wrapWorkerEntrypoint(klass) {
+  if (__INTERNAL_WRANGLER_MIDDLEWARE__ === void 0 || __INTERNAL_WRANGLER_MIDDLEWARE__.length === 0) {
+    return klass;
+  }
+  for (const middleware of __INTERNAL_WRANGLER_MIDDLEWARE__) {
+    __facade_register__(middleware);
+  }
+  return class extends klass {
+    #fetchDispatcher = /* @__PURE__ */ __name((request, env, ctx) => {
+      this.env = env;
+      this.ctx = ctx;
+      if (super.fetch === void 0) {
+        throw new Error("Entrypoint class does not define a fetch() function.");
+      }
+      return super.fetch(request);
+    }, "#fetchDispatcher");
+    #dispatcher = /* @__PURE__ */ __name((type, init) => {
+      if (type === "scheduled" && super.scheduled !== void 0) {
+        const controller = new __Facade_ScheduledController__(
+          Date.now(),
+          init.cron ?? "",
+          () => {
+          }
+        );
+        return super.scheduled(controller);
+      }
+    }, "#dispatcher");
+    fetch(request) {
+      return __facade_invoke__(
+        request,
+        this.env,
+        this.ctx,
+        this.#dispatcher,
+        this.#fetchDispatcher
+      );
+    }
+  };
+}
+__name(wrapWorkerEntrypoint, "wrapWorkerEntrypoint");
+var WRAPPED_ENTRY;
+if (typeof middleware_insertion_facade_default === "object") {
+  WRAPPED_ENTRY = wrapExportedHandler(middleware_insertion_facade_default);
+} else if (typeof middleware_insertion_facade_default === "function") {
+  WRAPPED_ENTRY = wrapWorkerEntrypoint(middleware_insertion_facade_default);
+}
+var middleware_loader_entry_default = WRAPPED_ENTRY;
+export {
+  __INTERNAL_WRANGLER_MIDDLEWARE__,
+  middleware_loader_entry_default as default
+};
+//# sourceMappingURL=index.js.map
---
+++ index.js
@@ -0,0 +1,85 @@
+async function handleRequest(request, env) {
+  const url = new URL(request.url);
+  let imagePrompt = url.searchParams.get('t');  // 获取生成图像的描述
+  
+  if (!imagePrompt) {
+    return new Response('miss params', { status: 400 });
+  }
+
+  // 检查 KV 存储中是否已有图像
+  let imageBuffer = await env.IMAGE_KV.get('image-' + imagePrompt);
+
+  if (!imageBuffer) {
+    //翻译优化
+    try{
+      let transRes = await translate(imagePrompt,env);
+      console.log(transRes);
+      imagePrompt = transRes.result.translated_text
+    }catch(e){
+
+      console.log('翻译错误:')
+      console.log(e)
+    }
+    // 如果没有缓存的图像，生成图像并存储
+    console.log('提示词：',imagePrompt)
+    imageBuffer = await generateImageWithCloudflare(imagePrompt, env);
+  }
+
+  // 返回图像二进制数据，浏览器会直接显示图像
+  return new Response(imageBuffer, {
+    headers: { 'Content-Type': 'image/png', 'Cache-Control': 'public, max-age=1800' }, 
+  });
+}
+
+//@cf/meta/m2m100-1.2b
+async function translate(text,env,input,output) {
+  
+  return await fetch(`https://api.cloudflare.com/client/v4/accounts/${env.account_id}/ai/run/@cf/meta/m2m100-1.2b`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${env.SF_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      text: text,
+      source_lang: input ? input : 'chinese',
+      target_lang: output ? output : 'english',
+    }),
+  }).then((res)=>res.json());
+}
+
+async function generateImageWithCloudflare(prompt, env) {
+  // 使用 Cloudflare 的图像生成模型进行图像生成
+  const response = await fetch(`https://api.cloudflare.com/client/v4/accounts/${env.account_id}/ai/run/${env.CF_IMG2TEXT_MODEL}`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${env.SF_TOKEN}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      model: env.CF_IMG2TEXT_MODEL,  // 使用 Cloudflare 的模型生成图像
+      prompt: prompt,
+      // num_steps: env.FLUX_NUM_STEPS
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error('生成图像失败');
+  }
+
+  // 获取返回的二进制图像内容
+  const imageBuffer = await response.arrayBuffer();
+
+  // 使用 `prompt` 作为唯一的 `key`，确保相同的提示词返回相同的图像
+  const key = `image-${prompt}`;  // 使用提示词作为 key
+  await env.IMAGE_KV.put(key, imageBuffer, { expirationTtl: 1800 });
+
+  // 返回图像的二进制数据
+  return imageBuffer;
+}
+
+export default {
+  async fetch(request, env, ctx) {
+    return handleRequest(request, env);  // 直接返回图像的二进制数据
+  },
+};
